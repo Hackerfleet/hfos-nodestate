c14385b90da88ca09266c8f1aa350c0e6e533abc
ri0t <riot@c-base.org>
2019-03-09 18:29:48 +0100

    Fixed packaging, added Trove classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 # HFOS - Hackerfleet Operating System
 # ===================================
-# Copyright (C) 2011-2018 Heiko 'riot' Weinen <riot@c-base.org> and others.
+# Copyright (C) 2011-2019 Heiko 'riot' Weinen <riot@c-base.org> and others.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -23,32 +23,49 @@ __license__ = "AGPLv3"
 
 from setuptools import setup, find_packages
 
-setup(name="hfos-nodestate",
-      version="0.0.1",
-      description="hfos-nodestate",
-      author="Hackerfleet Community",
-      author_email="riot@c-base.org",
-      url="https://github.com/hackerfleet/hfos-nodestate",
-      license="GNU Affero General Public License v3",
-      packages=find_packages(),
-      long_description="""HFOS - Nodestate
+setup(
+    name="hfos-nodestate",
+    version="0.0.1",
+    description="hfos-nodestate",
+    author="Hackerfleet Community",
+    author_email="riot@c-base.org",
+    url="https://github.com/hackerfleet/hfos-nodestate",
+    license="GNU Affero General Public License v3",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Isomer :: 1',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""HFOS - Nodestate
 ================
 
 A module to toggle and trigger node wide state flags.
 
 This software package is a plugin module for HFOS.
 """,
-      dependency_links=[
-      ],
-      install_requires=[
-          'isomer>=1.0.0',
-      ],
-      entry_points="""[isomer.components]
-    nodestate=hfos.nodestate.manager:Nodestate
+    dependency_links=[
+    ],
+    install_requires=[
+        'isomer>=1.0.0',
+    ],
+    entry_points="""[isomer.components]
+    nodestate=isomer.nodestate.manager:Nodestate
     [isomer.schemata]
-    nodestate=hfos.nodestate.nodestate:NodeState
+    nodestate=isomer.nodestate.nodestate:NodeState
     [isomer.provisions]
-    nodestates=hfos.nodestate.provisions.nodestates:provision
+    nodestates=isomer.nodestate.provisions.nodestates:provision
     """,
-      test_suite="tests.main.main",
-      )
+    test_suite="tests.main.main",
+)
